54072d024f9b43e9d53a036512c172fabc4e2bb5
Anshuman Rohella via macbook <rohella.anshuman@gmail.com>
2017-12-14 14:09:47 +0900

    added dialog to the client

--- WTGDemoClient.java
+++ WTGDemoClient.java
@@ -1,3 +1,4 @@
+
 package presto.android.gui.clients;
 
 import com.google.common.collect.Iterables;
@@ -47,176 +48,187 @@ import java.util.regex.Pattern;
  * Created by zero on 10/21/15.
  */
 public class WTGDemoClient implements GUIAnalysisClient {
-  
-  @Override
-  public void run(GUIAnalysisOutput output){
     
-    try{
-        File file = new File("/Users/anshuman/Documents/workspace/BeginImplementation/WTG/WTG.xml");
+    @Override
+    public void run(GUIAnalysisOutput output){
         
-       PrintStream out = new PrintStream(file);
-   /*
-      DocumentBuilderFactory dbFactory =DocumentBuilderFactory.newInstance();
-
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.newDocument();
-  */
+        try{
+            File file = new File("/Users/anshumanrohella/Documents/workspace/BeginImplementation/MunchLife-master/WTG/WTG.xml");
+            
+            PrintStream out = new PrintStream(file);
+            /*
+             DocumentBuilderFactory dbFactory =DocumentBuilderFactory.newInstance();
+             
+             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+             Document doc = dBuilder.newDocument();
+             */
+            
+            VarUtil.v().guiOutput = output;
+            WTGBuilder wtgBuilder = new WTGBuilder();
+            wtgBuilder.build(output);
+            WTGAnalysisOutput wtgAO = new WTGAnalysisOutput(output, wtgBuilder);
+            WTG wtg = wtgAO.getWTG();
+            
+            Collection<WTGEdge> edges = wtg.getEdges();
+            Collection<WTGNode> nodes = wtg.getNodes();
+            
+            
+            // Logger.verb("DEMO", "Application: " + Configs.benchmarkName);
+            //Logger.verb("DEMO", "Launcher Node: " + wtg.getLauncherNode());
+            //  pw.println(wtg.getLauncherNode().getWindow().toString());
+            
+            //pw.prinln(wtg.)
+            
+            out.print("<WindowsTransitionGraph>\n");
+            
+            for (WTGNode n : nodes){
+                
+                //    Element activity = doc.createElement("Activity");
+                String activity_name = actString(n.getWindow().toString());
+                out.println("\t<Activity name=\""+activity_name+"\" >");
+                
+                for(WTGEdge ed : n.getOutEdges()){
+                    if(actString(n.getWindow().toString()).contains(".Menu"))
+                    {
+                        if(String.valueOf(ed.getEventType()).compareTo("click")==0)
+                        {
+                            out.println("\t\t<View Type=\""+getEventType(ed.getGUIWidget().toString())+"\" Event=\""+ed.getEventType()+"\" Destination=\""+actString(ed.getTargetNode().getWindow().toString())+"\" ID=\""+getWidgetId(ed.getGUIWidget().toString())+"\" />");
+                        }
+                    }
+                    else
+                        out.println("\t\t<View Type=\""+getEventType(ed.getGUIWidget().toString())+"\" Event=\""+ed.getEventType()+"\" Destination=\""+actString(ed.getTargetNode().getWindow().toString())+"\" ID=\""+getWidgetId(ed.getGUIWidget().toString())+"\" />");
+                    
+                }
+                out.println("\t</Activity>");
+                
+            }
+            out.print("</WindowsTransitionGraph>\n");
+            out.flush();
+            out.close();
+            /*
+             TransformerFactory transformerFactory = TransformerFactory.newInstance();
+             Transformer transformer = transformerFactory.newTransformer();
+             DOMSource source = new DOMSource(doc);
+             StreamResult result = new StreamResult(new File("/Users/anshumanrohella/Documents/workspace/BeginImplementation/TippyTipper-master/WTG/WTG.xml"));
+             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+             transformer.transform(source, result);
+             */
+        }catch(Exception e){e.printStackTrace();}
+    }
     
-    VarUtil.v().guiOutput = output;
-    WTGBuilder wtgBuilder = new WTGBuilder();
-    wtgBuilder.build(output);
-    WTGAnalysisOutput wtgAO = new WTGAnalysisOutput(output, wtgBuilder);
-    WTG wtg = wtgAO.getWTG();
-
-    Collection<WTGEdge> edges = wtg.getEdges();
-    Collection<WTGNode> nodes = wtg.getNodes();
-
-
-   // Logger.verb("DEMO", "Application: " + Configs.benchmarkName);
-    //Logger.verb("DEMO", "Launcher Node: " + wtg.getLauncherNode());
-  //  pw.println(wtg.getLauncherNode().getWindow().toString());
-  
-    //pw.prinln(wtg.)
-    
-    out.print("<WindowsTransitionGraph>\n");
-    
-    for (WTGNode n : nodes){
-    
-    //  Element activity = doc.createElement("Activity");
-        out.println("\t<Activity name=\""+actString(n.getWindow().toString())+"\" >");
-
-      for(WTGEdge ed : n.getOutEdges()){
-        if(actString(n.getWindow().toString()).contains(".Menu"))
-        {
-          if(String.valueOf(ed.getEventType()).compareTo("click")==0)
-          {
-            out.println("\t\t<View Type=\""+getEventType(ed.getGUIWidget().toString())+"\" Event=\""+ed.getEventType()+"\" Destination=\""+actString(ed.getTargetNode().getWindow().toString())+"\" ID=\""+getWidgetId(ed.getGUIWidget().toString())+"\" />");
-          }
+    public String actString(String act_name){
+        if(act_name.contains("LAUNCHER")){
+            
+            return act_name;
         }
-        else
-          out.println("\t\t<View Type=\""+getEventType(ed.getGUIWidget().toString())+"\" Event=\""+ed.getEventType()+"\" Destination=\""+actString(ed.getTargetNode().getWindow().toString())+"\" ID=\""+getWidgetId(ed.getGUIWidget().toString())+"\" />");
-       
-      }
-      out.println("\t</Activity>");
-      
-    }
-    out.print("</WindowsTransitionGraph>\n");
-    out.flush();
-    out.close();
-/*
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer transformer = transformerFactory.newTransformer();
-    DOMSource source = new DOMSource(doc);
-    StreamResult result = new StreamResult(new File("/Users/anshumanrohella/Documents/workspace/BeginImplementation/TippyTipper-master/WTG/WTG.xml"));
-    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-    transformer.transform(source, result);
-   */
-  }catch(Exception e){e.printStackTrace();}
-  }
-  
-  public String actString(String act_name){
-    if(act_name.contains("LAUNCHER")){
-      
-      return act_name;
-    }
-    System.out.println("Received String :::"+act_name);
-    
-    String Pattern1= "";
-    String Pattern2="";
-    Matcher m = null;
-    String ret="";
-    try{
-    
-   
-    if(act_name.startsWith("ACT")){
-      
-       Pattern1 = "ACT[";
-       Pattern2 = "]";
-       Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
-       m = p.matcher(act_name);
-        m.find();
-        ret = m.group(1);
-      
-    }
-    else if(act_name.startsWith("OptionsMenu"))
-    {
-      Pattern1 = "OptionsMenu[";
-      Pattern2 =  "]";
-      Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
-      m = p.matcher(act_name);
-      m.find();
-      ret = m.group(1)+".Menu";
+        System.out.println("Received String :::"+act_name);
+        
+        String Pattern1= "";
+        String Pattern2="";
+        Matcher m = null;
+        String ret="";
+        try{
+            
+            
+            if(act_name.startsWith("ACT")){
+                
+                Pattern1 = "ACT[";
+                Pattern2 = "]";
+                Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
+                m = p.matcher(act_name);
+                m.find();
+                ret = m.group(1);
+                
+            }
+            else if(act_name.startsWith("DIALOG")){
+                Pattern1 = "DIALOG[";
+                Pattern2 = "]";
+                Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
+                m = p.matcher(act_name);
+                m.find();
+                ret = m.group(1);
+                
+            }
+            else if(act_name.startsWith("OptionsMenu"))
+            {
+                Pattern1 = "OptionsMenu[";
+                Pattern2 =  "]";
+                Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
+                m = p.matcher(act_name);
+                m.find();
+                ret = m.group(1)+".Menu";
+            }
+            
+            
+            
+        }catch(Exception e){e.printStackTrace();}
+        
+        return ret;
+        
     }
     
-    
-    
-    }catch(Exception e){e.printStackTrace();}
-    
-    return ret;
-    
-  }
-  
-  public String getEventType(String widget){
-    if(widget.contains("LAUNCHER")){
-      
-      return widget;
+    public String getEventType(String widget){
+        if(widget.contains("LAUNCHER")){
+            
+            return widget;
+        }
+        System.out.println("Received widget :::"+widget);
+        String ret ="";
+        String Pattern1= "";
+        String Pattern2="";
+        Matcher m = null;
+        try{
+            
+            if(widget.startsWith("INFL"))
+            {
+                Pattern1 = "INFL[";
+                Pattern2 =  ",";
+                
+                
+                
+                Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
+                m = p.matcher(widget);
+                m.find();
+                ret = m.group(1);
+            }
+            else
+                ret ="android.view.MenuItem";
+            
+        }catch(Exception e){e.printStackTrace();}
+        
+        return ret;
+        
     }
-    System.out.println("Received widget :::"+widget);
-    String ret ="";
-    String Pattern1= "";
-    String Pattern2="";
-    Matcher m = null;
-    try{
-
-    if(widget.startsWith("INFL"))
-    {
-      Pattern1 = "INFL[";
-      Pattern2 =  ","; 
-     
-    
-  
-    Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
-    m = p.matcher(widget);
-    m.find();
-    ret = m.group(1);
+    public String getWidgetId(String widget){
+        if(widget.contains("LAUNCHER")){
+            
+            return widget;
+        }
+        System.out.println("Received widget :::"+widget);
+        String ret = null;
+        String Pattern1= "";
+        String Pattern2="";
+        Matcher m = null;
+        try{
+            if(widget.startsWith("INFL") || widget.startsWith("MenuItemINFL"))
+            {
+                Pattern1 = "|";
+                Pattern2 =  "]";
+                Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
+                m = p.matcher(widget);
+                m.find();
+                ret = m.group(1);
+                
+            }
+            
+            
+            
+            
+        }catch(Exception e){e.printStackTrace(); return "nullId";}
+        
+        return ret;
+        
     }
-    else
-      ret ="android.view.MenuItem";
     
-    }catch(Exception e){e.printStackTrace();}
-    
-    return ret;
-    
-  }
-  public String getWidgetId(String widget){
-    if(widget.contains("LAUNCHER")){
-      
-      return widget;
-    }
-    System.out.println("Received widget :::"+widget);
-    String ret = null;
-    String Pattern1= "";
-    String Pattern2="";
-    Matcher m = null;
-try{
-    if(widget.startsWith("INFL") || widget.startsWith("MenuItemINFL"))
-    {
-      Pattern1 = "|";
-      Pattern2 =  "]"; 
-      Pattern  p = Pattern.compile(Pattern.quote(Pattern1)+"(.*?)"+Pattern.quote(Pattern2));
-      m = p.matcher(widget);
-      m.find();
-      ret = m.group(1);
-      
-    }
-
-    
-    
-    
-    }catch(Exception e){e.printStackTrace(); return "nullId";}
-    
-    return ret;
-    
-  }
-  
 }
+
